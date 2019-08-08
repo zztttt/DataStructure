778e44bcfd32ce0747e1f2805c9cc5a46622da53
zztttt <1315691850@qq.com>
2019-08-08 17:06:15 +0800

    update binary tree

--- BinaryTree/BinaryTree.h
+++ BinaryTree/BinaryTree.h
@@ -1,6 +1,9 @@
 #ifndef BINARYTREE_H_
 #define BINARYTREE_H_
 #include<assert.h>
+#include<algorithm>
+
+using std::max;
 
 #define stature(p) ((p)? (p)->height : -1)
 
@@ -22,7 +25,8 @@
 		(x)->parent->parent->lc )
 #define FromParentTo(x)\
 	( IsRoot(x)? _root: \
-	             (IsLChild(x)? (x).parent->lc:(x)->parent->lc) )
+	             (IsLChild(x)? (x).parent->lc: \
+                               (x).parent->rc) )
 
 enum RBColor {
 	RB_RED, 
@@ -60,17 +64,33 @@ public:
 	bool operator< (TreeNode const& node);
 	bool operator== (TreeNode const& node);
 };
+
+template<class T>
+class Tree {
+protected:
+	int _size;
+	TreeNode<T>* _root;
+	virtual int updateHeight(TreeNode<T>* x);
+	void updateHeightAbove(TreeNode<T>* x);
+public:
+	Tree();
+	~Tree();
+	int size() const;
+	bool empty() const;
+	TreeNode<T>* getRoot() const;
+
+	TreeNode<T>* insertAsRoot(T const& e);
+	TreeNode<T>* insertAsLeftChild(TreeNode<T>* x, T const& e);
+	TreeNode<T>* insertAsRightChild(TreeNode<T>* x, T const& e);
+	TreeNode<T>* attachAsLeftChild(TreeNode<T>* x, Tree<T>* &T);
+	TreeNode<T>* attachAsRightChild(TreeNode<T>* x, Tree<T>* &T);
+	int remove(TreeNode<T>* x);
+
+	Tree<T>* secede(TreeNode<T>* x);
+};
 #endif
 /*-------------------------------------------------------*/
 /*----------------------TreeNode-------------------------*/
-
-
-template<class T>
-inline int TreeNode<T>::size() {
-	assert(0);
-	//TODO
-	return 0;
-}
 
 template<class T>
 TreeNode<T>* TreeNode<T>::insertAsLeftChild(T const & e) {
@@ -94,3 +114,132 @@ bool TreeNode<T>::operator==(TreeNode const & node) {
 
 /*--------------------------------------------------------*/
 /*-----------------------Tree-----------------------------*/
+
+template<class T>
+inline int Tree<T>::updateHeight(TreeNode<T>* x) {
+	return x->height = 1 + max(stature(x->lc), stature(x->rc));
+}
+
+template<class T>
+inline void Tree<T>::updateHeightAbove(TreeNode<T>* x) {
+	while (x) {
+		updateHeight(x);
+		x = x->parent;
+	}
+}
+
+template<class T>
+inline Tree<T>::Tree() {
+	_size = 0;
+	_root = NULL;
+}
+
+template<class T>
+inline Tree<T>::~Tree() {
+	if (0 < _size)
+		remove(_root);
+}
+
+template<class T>
+inline int Tree<T>::size() const {
+	return _size;;
+}
+
+template<class T>
+inline bool Tree<T>::empty() const {
+	return !_root;
+}
+
+template<class T>
+inline TreeNode<T>* Tree<T>::getRoot() const {
+	return _root;
+}
+
+template<class T>
+inline TreeNode<T>* Tree<T>::insertAsRoot(T const & e) {
+	_size = 1;
+	_root = new TreeNode<T>(e);
+	return _root;
+}
+
+template<class T>
+inline TreeNode<T>* Tree<T>::insertAsLeftChild(TreeNode<T>* x, T const & e) {
+	
+	_size++;
+	x->insertAsLeftChild(e);
+	updateHeightAbove(x);
+	return x->lc;
+}
+
+template<class T>
+inline TreeNode<T>* Tree<T>::insertAsRightChild(TreeNode<T>* x, T const & e) {
+	_size++;
+	x->insertAsRightChild(e);
+	updateHeightAbove(x);
+	return x->rc;
+}
+
+/*
+* Usage: add tree t as the node x's left child.
+*        it requires x->lc == NULL
+* return 
+*/
+template<class T>
+inline TreeNode<T>* Tree<T>::attachAsLeftChild(TreeNode<T>* x, Tree<T>*& t) {
+	// 不懂为啥要这么写。。。 TODO
+	if (x->lc = t->_root)
+		x->lc->parent = x;
+	_size += t->_size;
+	updateHeightAbove(x);
+	t->_root = NULL;
+	t->_size = 0;
+	//release(t);
+	// avoid wild pointer
+	t = NULL;
+	return x;
+}
+
+template<class T>
+inline TreeNode<T>* Tree<T>::attachAsRightChild(TreeNode<T>* x, Tree<T>*& t) {
+	if (x->rc = t->_root)
+		x->rc->parent = x;
+	_size += t->_size;
+	updateHeightAbove(x);
+	t->_root = NULL;
+	t->_size = 0;
+	//release(t);
+	t = NULL;
+	return x;
+}
+
+/*
+* Usage: remove TreeNode* x in the tree
+* return the number of node of the subtree
+*/
+template<class T>
+inline int Tree<T>::remove(TreeNode<T>* x) {
+	// (*x).parent->l/rc = NULL
+	// cut the line: parent->lc = x
+	FromParentTo(*x) = NULL;
+	updateHeightAbove(x->parent);
+	int n = removeAt(x);
+	_size = _size - n;
+	return n;
+}
+
+/*-----------------------------------------------*/
+/*-------------------Untility--------------------*/
+
+/*
+* Usage: remove TreeNode x and its heirs
+* return the number of deleted nodes
+*/
+template<class T>
+static int removeAt(TreeNode<T>* x) {
+	if (!x)
+		return 0;
+	int n = 1 + removeAt(x->lc) + removeAt(x->rc);
+	//release(x->data);
+	//release(x);
+	return n;
+}
--- BinaryTree/BinaryTreeUnitTest.h
+++ BinaryTree/BinaryTreeUnitTest.h
@@ -34,8 +34,148 @@ namespace UnitTest {
 
 			Assert::AreEqual(sibling(lc)->data, 2);
 			Assert::AreEqual(sibling(rc)->data, 1);
-			// TODO: how to use FromParentTo
 		}
+		TEST_METHOD(InitTree) {
+			/*              1
+			*            2      3
+			*          4   5  6   7
+			*
+			*/
+			Tree<int> tree = Tree<int>();
+			Assert::AreEqual(tree.size(), 0);
+			Assert::IsTrue(tree.empty());
 
+			tree.insertAsRoot(1);
+			Assert::IsFalse(tree.empty());
+
+			TreeNode<int>* root = tree.getRoot();
+			tree.insertAsLeftChild(root, 2);
+			tree.insertAsRightChild(root, 3);
+			Assert::AreEqual(tree.size(), 3);
+			tree.insertAsLeftChild(root->lc, 4);
+			tree.insertAsRightChild(root->lc, 5);
+			tree.insertAsLeftChild(root->rc, 6);
+			tree.insertAsRightChild(root->rc, 7);
+
+			// check size and height
+			Assert::AreEqual(tree.size(), 7);
+			Assert::AreEqual(root->height, 2);
+			Assert::AreEqual(root->lc->height, 1);
+			Assert::AreEqual(root->rc->height, 1);
+			Assert::AreEqual(root->lc->rc->height, 0);
+			Assert::AreEqual(root->rc->lc->height, 0);
+
+			// check value
+			TreeNode<int> mask1 = TreeNode<int>(1);
+			TreeNode<int> mask2 = TreeNode<int>(2);
+			TreeNode<int> mask3 = TreeNode<int>(3);
+			TreeNode<int> mask4 = TreeNode<int>(4);
+			TreeNode<int> mask5 = TreeNode<int>(5);
+			TreeNode<int> mask6 = TreeNode<int>(6);
+			TreeNode<int> mask7 = TreeNode<int>(7);
+			Assert::IsTrue(*root == mask1);
+			Assert::IsTrue(*root->lc == mask2);
+			Assert::IsTrue(*root->rc == mask3);
+			Assert::IsTrue(*root->lc->lc == mask4);
+			Assert::IsTrue(*root->lc->rc == mask5);
+			Assert::IsTrue(*root->rc->lc == mask6);
+			Assert::IsTrue(*root->rc->rc == mask7);
+		}
+		TEST_METHOD(InsertAndRemove) {
+			/*              1
+			*            2      3
+			*          4   5  6   7
+			*
+			*/
+			// init
+			Tree<int> tree = Tree<int>();
+			tree.insertAsRoot(1);
+			TreeNode<int>* root = tree.getRoot();
+			tree.insertAsLeftChild(root, 2);
+			tree.insertAsRightChild(root, 3);
+			Assert::AreEqual(tree.size(), 3);
+			tree.insertAsLeftChild(root->lc, 4);
+			tree.insertAsRightChild(root->lc, 5);
+			tree.insertAsLeftChild(root->rc, 6);
+			tree.insertAsRightChild(root->rc, 7);
+
+			// remove
+			/* after remove
+			*              1
+			*            2
+			*/
+			Assert::AreEqual(tree.remove(root->lc->lc), 1);
+			Assert::AreEqual(tree.remove(root->lc->rc), 1);
+			Assert::AreEqual(tree.remove(root->rc), 3);
+			Assert::AreEqual(root->height, 1);
+			Assert::AreEqual(root->lc->height, 0);
+			Assert::IsNull(root->rc);
+			Assert::AreEqual(tree.size(), 2);
+
+			// insert again
+			tree.insertAsRightChild(root, 3);
+			tree.insertAsLeftChild(root->lc, 4);
+			tree.insertAsRightChild(root->lc, 5);
+			tree.insertAsLeftChild(root->rc, 6);
+			tree.insertAsRightChild(root->rc, 7);
+
+			// check again
+			// check size and height
+			Assert::AreEqual(tree.size(), 7);
+			Assert::AreEqual(root->height, 2);
+			Assert::AreEqual(root->lc->height, 1);
+			Assert::AreEqual(root->rc->height, 1);
+			Assert::AreEqual(root->lc->rc->height, 0);
+			Assert::AreEqual(root->rc->lc->height, 0);
+
+			// check value
+			TreeNode<int> mask1 = TreeNode<int>(1);
+			TreeNode<int> mask2 = TreeNode<int>(2);
+			TreeNode<int> mask3 = TreeNode<int>(3);
+			TreeNode<int> mask4 = TreeNode<int>(4);
+			TreeNode<int> mask5 = TreeNode<int>(5);
+			TreeNode<int> mask6 = TreeNode<int>(6);
+			TreeNode<int> mask7 = TreeNode<int>(7);
+			Assert::IsTrue(*root == mask1);
+			Assert::IsTrue(*root->lc == mask2);
+			Assert::IsTrue(*root->rc == mask3);
+			Assert::IsTrue(*root->lc->lc == mask4);
+			Assert::IsTrue(*root->lc->rc == mask5);
+			Assert::IsTrue(*root->rc->lc == mask6);
+			Assert::IsTrue(*root->rc->rc == mask7);
+		}
+		TEST_METHOD(Large) {
+			Tree<int> tree = Tree<int>();
+			tree.insertAsRoot(1);
+			TreeNode<int>* root = tree.getRoot();
+			TreeNode<int>* p = root;
+
+			int loop = 10;
+			int base = 1000;
+			for (int i = 1; i <= loop; i++) {
+				// insert
+				for (int j = 0; j < i*base; j++) {
+					while (!IsLeaf(*p)) {
+						p = HasLChild(*p) ? p->lc : p->rc;
+					}
+					tree.insertAsLeftChild(p, j);
+				}
+
+				// check
+				Assert::AreEqual(tree.size(), i*base + 1);
+
+				// remove
+				p = root;
+				for (int j = 0; j < i*base; j++) {
+					while (!IsLeaf(*p)) {
+						p = HasLChild(*p) ? p->lc : p->rc;
+					}
+					tree.remove(p);
+				}
+
+				// check again
+				Assert::AreEqual(tree.size(), 1);
+			}
+		}
 	};
 }
